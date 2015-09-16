57d56e7fb87e9d74b7ed14aec769ff5d8bb717e0
cancerogen16 <don544@mail.ru>
2015-09-16 12:08:15 +0500

    edit

--- shop/index_http:/avangard.biz/shop.php.php
+++
@@ -1,2 +0,0 @@
-<link rel="File-List" href="file:///C:\DOCUME~1\nick\LOCALS~1\Temp\msohtmlclip1\01\clip_filelist.xml"></link><link rel="themeData" href="file:///C:\DOCUME~1\nick\LOCALS~1\Temp\msohtmlclip1\01\clip_themedata.thmx"></link><link rel="colorSchemeMapping" href="file:///C:\DOCUME~1\nick\LOCALS~1\Temp\msohtmlclip1\01\clip_colorschememapping.xml"></link><span style="font-size: 8pt; line-height: 115%;"><o:p></o:p></span> <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?> <img src="/upload/iblock/709/main_20_188x115.gif" /> 
-<br />
